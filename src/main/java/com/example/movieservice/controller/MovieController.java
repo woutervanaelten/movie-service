e2cdfd67362d3ab3daf6538d338b17e8d51ba66d
woutervanaelten <r0748324@student.thomasmore.be>
2020-12-28 14:01:35 +0100

    CI/CD

--- src/main/java/com/example/movieservice/controller/MovieController.java
+++ src/main/java/com/example/movieservice/controller/MovieController.java
@@ -11,7 +11,7 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
-public class BookController {
+public class MovieController {
 
     @Autowired
     private MovieRepository movieRepository;
